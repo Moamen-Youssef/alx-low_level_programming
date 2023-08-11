4968d1a2fb48b0e5dcdc644ad76b8a07815bc6e1
Moamen-Youssef <momenyoussef1998@gmail.com>
2023-08-10 19:56:44 -0700

    f

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
-/**                                                                                       
- * main - printing some text 
- * main - Prints "Programming is
- like building a multilingual puzzle"
- * Return:0  
+/**
+ * main - printing some text
+ * main - Prints "Programming islike building"
+ * Return:0
  */
 int main(void)
 {
 puts(
-"Programming is like buildinga multilingual puzzle"
-"\n");
+"\"Programming is like buildinga multilingual puzzle\n"
+    );
 return (0);
 }
 
