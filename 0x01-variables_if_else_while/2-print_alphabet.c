a3fc7bb830202e61992b4183041d8b0fe56e3174
Moamen-Youssef <momenyoussef1998@gmail.com>
2023-08-11 15:53:16 -0700

    to task 5

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,10 +8,7 @@ int main()
 char ch;
 for (ch = 'a'; ch <= 'z'; ch++)
 {
-if (ch != 'e' && ch != 'q')
-{
 putchar(ch);
-}
 }
 putchar('\n');
 return (0);
--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,10 +8,11 @@ int main()
 char ch;
 for (ch = 'a'; ch <= 'z'; ch++)
 {
-if (ch != 'e' && ch != 'q')
-{
 putchar(ch);
 }
+for (ch = 'A'; ch <= 'Z'; ch++)
+{
+putchar(ch);
 }
 putchar('\n');
 return (0);
