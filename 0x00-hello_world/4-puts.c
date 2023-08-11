ccd9e41eabf1180bfb1909a47bc24a5701b96e01
Moamen-Youssef <momenyoussef1998@gmail.com>
2023-08-10 18:24:54 -0700

    try 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,23 @@
+ #include <stdio.h>
+/**                                                                                        
+ * main - printing some text                                                               
+ * main - Prints "Programming is like building a multilingual puzzle"
+ * Return:0                                                                                
+ */
+int main(void)
+{
+char mystr[50] ;
+mystr = "Programming is like building a multilingual puzzle";
+puts(mystr);
+return (0);
+}
+
+
+
+
+
+
+
+
+
+
