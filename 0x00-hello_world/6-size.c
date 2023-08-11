09654ebd757fb8e12dd4d0c0eacd684796e4425e
Moamen-Youssef <momenyoussef1998@gmail.com>
2023-08-11 05:08:01 -0700

    f

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,7 +11,7 @@ int int_type;
 long int long_int_type;
 long long int long_long_int_type;
 float float_type;
-printf("Size of a char: %c byte(s)\n",
+printf("Size of a char: %u byte(s)\n",
 sizeof(char_type));
 printf("Size of an int: %u byte(s)\n",
 sizeof(int_type));
@@ -19,7 +19,7 @@ printf("Size of a long int: %lu byte(s)\n",
 sizeof(long_int_type));
 printf("Size of a long long int: %lu byte(s)\n",
 sizeof(long_long_int_type));
-printf("Size of a flaot: %f byte(s)\n",
+printf("Size of a flaot: %u byte(s)\n",
 sizeof(float_type));
 return (0);
 }
