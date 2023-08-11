664709e6f690d45d7262686744e8d467a3a01154
Moamen-Youssef <momenyoussef1998@gmail.com>
2023-08-10 19:28:27 -0700

    g

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,7 @@
  */
 int main(void)
 {
-char mystr;
-mystr = "Programming is like building a multilingual puzzle";
+char mystr = "Programming is like building a multilingual puzzle";
 puts(mystr);
 return (0);
 }
