c9aebc933dc43daaa8446c9aeae1a4efe63f3ba7
Moamen-Youssef <momenyoussef1998@gmail.com>
2023-08-15 01:15:14 -0700

    to task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print alphabet
+ * Return:0
+*/
+void print_alphabet(void)
+{
+char alpha;
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+{
+printf("%c" , alpha);
+}
+printf("\n");
+return;
+}
+
+int main (void)
+{
+print_alphabet();
+return(0);
+}
