8a84f09c76ca6359a5b72de58db37ae485249dc9
Moamen-Youssef <momenyoussef1998@gmail.com>
2023-08-17 21:21:01 -0700

    g

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - check if the char is upper
+ * @c: the char
+ * Return:0or1
+*/
+int _isupper(int c)
+{
+if (isupper(c))
+{
+return (1);
+}
+else 
+{
+return (0);
+}
+}
