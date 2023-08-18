282958bf347cb34d3942e6bf8f48f78fc17d0957
Moamen-Youssef <momenyoussef1998@gmail.com>
2023-08-17 23:48:48 -0700

    f

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,15 +8,21 @@ int main(void)
 int i;
 for (i = 1; i <= 100; i++)
 {
-if (i % 3 == 1)
+if(i % 5 == 0 && i % 3 == 0)
+{
+printf("FiZZBuzz ");
+}
+else if (i % 3 == 0)
 {
 printf("Fizz ");
 }
-if(i % 5)
+else if(i % 5 == 0)
 {
 printf("Buzz ");
 }
-printf("%d ", i);
+else
+{
+printf("%d ", i);   
 }
-printf("\n");
+}
 }
