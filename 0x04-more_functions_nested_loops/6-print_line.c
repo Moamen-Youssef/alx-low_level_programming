7bb49baa89d751dbe0dadfd554e4e98086f0decb
Moamen-Youssef <momenyoussef1998@gmail.com>
2023-08-17 22:59:42 -0700

    d

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
  * print_line - print straigt line
+ * @n: the number of \'s
  * Return:nothing
 */
 void print_line(int n)
 {
 int i;
-for (i =0; i < n; i++ )
+for (i = 0; i < n; i++ )
 {
 if (n > 0) 
 _putchar('_');
