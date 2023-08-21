659df3ff2fb071b5ceb3154d10940c11a5edf074
Moamen-Youssef <momenyoussef1998@gmail.com>
2023-08-21 12:38:08 -0700

    dam

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,11 +6,13 @@
  * Return:nothing.
  */
 
-void print_rev(char *s){
+void print_rev(char *s)
+{
 int strlength = strlen(s);
 int i;
 for (i = strlength; i > 0; i--)
 {
 printf("%c", s[i - 1]);
 }
+printf("\n");
 }
