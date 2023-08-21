04e5ecc9d0a513a29b1478840288c0ebdda6ea4a
Moamen-Youssef <momenyoussef1998@gmail.com>
2023-08-21 15:58:47 -0700

    ffs

---
+++ 0x05-pointers_arrays_strings/9-strcpy.cc
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcpy - copy string
+ * @dest: the pointer to the variable will store the string
+ * @src: pointer to the string to copy
+ * Return:the pointer to dest.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+strcpy(dest , src);
+return (dest);
+}
