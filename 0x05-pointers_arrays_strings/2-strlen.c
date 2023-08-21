f9a3e203b45899e4c1eeb522c80d0eb0679a6652
Moamen-Youssef <momenyoussef1998@gmail.com>
2023-08-21 10:57:25 -0700

    f

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - return the string length
+ * @s: the string
+ * Return:nothing.
+ */
+
+int _strlen(char *s)
+{
+return("%d", strlen(s));
+}
