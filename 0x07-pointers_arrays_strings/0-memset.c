3e11d2f6604de295f5f9759fd8574b835401c723
Moamen-Youssef <113758180+Moamen-Youssef@users.noreply.github.com>
2023-08-29 03:12:38 +0300

    Update 0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _memset - add value to memory location
+ * @s: the pointer of the memory to be filled
+ * @b: the char
+ * @n:the number of rep.
+ * Return: pointer.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+memset(s, b, n);
+return (s);
+}
