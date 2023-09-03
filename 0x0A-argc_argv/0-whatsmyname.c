57f1c542690bced19a17dc07a0640e8c1639b25d
Moamen-Youssef <113758180+Moamen-Youssef@users.noreply.github.com>
2023-09-03 10:43:48 +0300

    f

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,12 +4,12 @@
  * main - function types its name
  * @argc: the command line arguments number
  * @argv: the array of command line strings
- * Return: . 
+ * Return: 0. 
 */
 
 int main (int argc, char *argv[])
 {
 (void)argc;
-printf("%s", argv[0]);
+printf("%s\n", argv[0]);
 return (0);
 }
