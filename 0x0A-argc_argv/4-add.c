c5d9cf7b5caf9dbdb1cb473d135b1779e9fcc015
Moamen-Youssef <113758180+Moamen-Youssef@users.noreply.github.com>
2023-09-03 11:30:44 +0300

    finish

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,16 +10,15 @@
 
 int main (int argc, char *argv[])
 {
-int i, sum = 0, check;
+int i, sum = 0;
 
 for (i = 1; i < argc; i++)
 {
-check = isdigit(atoi(argv[i]));
-if (check)
+if (isdigit(*argv[i]) != 0)
 {
 sum = sum + atoi(argv[i]);
 }
-else 
+else
 {
 printf("Error\n");
 return (1);
