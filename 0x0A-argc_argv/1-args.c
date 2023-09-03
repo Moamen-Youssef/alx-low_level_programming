2c7a201d734e97ef5aa62a25937f9b13f9447183
Moamen-Youssef <113758180+Moamen-Youssef@users.noreply.github.com>
2023-09-03 18:51:12 +0300

    d

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,14 +2,14 @@
 
 /**
  * main - function types its arguments number
- * @argc: the command line arguments number
- * @argv: the array of command line strings
+ * @argc:the command line arguments number
+ * @argv:the array of command line strings
  * Return: 0.
 */
 
-int main (int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-(void) argv;
+(void)argv;
 printf("%d\n", argc - 1);
 return (0);
 }
