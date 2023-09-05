05c5ab1e381ffe55443de9da3745e4b92b1d1589
Moamen-Youssef <113758180+Moamen-Youssef@users.noreply.github.com>
2023-09-06 00:22:46 +0300

    mallocc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * alloc_grid - prints a grid of integers
+ * print_grid - prints a grid of integers
  * @width: width of the grid
  * @height: height of the grid
  * Return: Nothing.
