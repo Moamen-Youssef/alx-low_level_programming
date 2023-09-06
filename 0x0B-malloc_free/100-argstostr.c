67bad8ae6d178b52c8e1a138ce4eeb62db70d486
Moamen-Youssef <113758180+Moamen-Youssef@users.noreply.github.com>
2023-09-06 11:25:08 +0300

    advanced

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -39,17 +39,3 @@ strcat(str, av[i]);
 }
 return (str);
 }
-
-int main(int ac, char *av[])
-{
-    char *s;
-
-    s = argstostr(ac, av);
-    if (s == NULL)
-    {
-        return (1);
-    }
-    printf("%s", s);
-    free(s);
-    return (0);
-}
