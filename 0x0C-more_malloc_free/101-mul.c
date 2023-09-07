3d86514377c6abed1661023cf69da0f463168c4e
Moamen-Youssef <113758180+Moamen-Youssef@users.noreply.github.com>
2023-09-07 17:59:38 +0300

    more malloc

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code
+ *@argc: ....
+ *@argv: ....
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+int mul;
+if (argc > 3)
+{
+printf("Error\n");
+exit(98);
+}
+mul = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", mul);
+return (mul);
+}
