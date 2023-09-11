555e6fc1bd84594b08f0ce50823c7a3b69c5615e
Moamen-Youssef <113758180+Moamen-Youssef@users.noreply.github.com>
2023-09-12 02:46:52 +0300

    typedef and struct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -6,12 +6,14 @@
  *@name: name
  *@age: age
  *@owner: owner
- * Return: nothing.
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+if (d != NULL)
+{
 d->name = name;
 d->age = age;
 d->owner = owner;
+}
 }
