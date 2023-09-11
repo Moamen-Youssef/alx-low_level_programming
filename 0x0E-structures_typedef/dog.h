d5f0278b9e6ec93d3adfbef1a6d686a41497bc8c
Moamen-Youssef <113758180+Moamen-Youssef@users.noreply.github.com>
2023-09-12 02:30:21 +0300

    macros

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog data
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner of the dog
+ *
+ * Description:the dog and type dog's struct
+ */
+
+struct dog 
+{
+const char* name;
+float age;
+const char* owner;
+};
+
+#endif
