2209ce4719b8669037ccb081b3f8eed611e34feb
Moamen-Youssef <113758180+Moamen-Youssef@users.noreply.github.com>
2023-09-12 02:44:16 +0300

    typedef and struct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,18 +1,18 @@
+#include <stdlib.h>
 #include "dog.h"
-
 /**
- * init_dog - check the code
- *@d: structure dog
- *@name: name
- *@age: age
- *@owner: owner
- * Return: Alway.
- */
-
-void init_dog(struct dog *d, char *name, 
-float age, char *owner)
+  * init_dog - initialize struct dog.
+  * @d: name of struct
+  * @name: name of dog in struct
+  * @age: age of dog in struct
+  * @owner: name of owner of dog in struct
+  */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d)
 {
 d->name = name;
 d->age = age;
 d->owner = owner;
+}
 }
