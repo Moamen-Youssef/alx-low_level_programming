a25c8bae4db574b51a6fcad4fdc4a89c3fae722c
Moamen-Youssef <113758180+Moamen-Youssef@users.noreply.github.com>
2023-09-13 21:49:42 +0300

    functios pointers

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - check if a number is
+ * @array: the array
+ * @size: the integer to check
+ * @cmp: function pointer
+ * Return:c.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+if (array != NULL && cmp != NULL)
+{
+if (size <= 0)
+{
+return (-1);
+}
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]))
+{
+return (i);
+}
+}
+return (-1);
+}
+}
