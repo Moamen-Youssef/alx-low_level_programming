aa8a4632719922c747f7022f309912503b793d05
Moamen-Youssef <113758180+Moamen-Youssef@users.noreply.github.com>
2023-10-02 21:57:26 +0300

    i\o

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <unistd.h>
 #include <stdlib.h>
 /**
  * create_file - check the code
@@ -9,13 +10,13 @@
 
 int create_file(const char *filename, char *text_content)
 {
-int file;
+int file, bytes = 0;
 int write_to;
 if (!filename)
 {
 return (-1);
 }
-file = open(filename, O_WRONLY | O_CREAT);
+file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 if (file == -1)
 {
 return (-1);
@@ -25,7 +26,12 @@ if (text_content == NULL)
 close(file);
 return (1);
 }
-write_to = write(STDOUT_FILENO, text_content, file);
+while (text_content[0] != "\0")
+{
+bytes++;
+}
+
+write_to = write(STDOUT_FILENO, text_content, bytes);
 if (write_to == -1)
 {
 return (-1);
