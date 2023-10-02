01d506a501865304b3d9452f6f8fd9c49f279d4d
Moamen-Youssef <113758180+Moamen-Youssef@users.noreply.github.com>
2023-10-02 22:11:20 +0300

    i\o

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,24 +10,26 @@
 
 int create_file(const char *filename, char *text_content)
 {
-int file, bytes = 0;
-if (!filename)
-{
-return (-1);
-}
-if (text_content == NULL)
-{
-text_content == "";
-}
-while (text_content[bytes] != '\0')
-{
-bytes++;
-}
-file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-if (file == -1)
-{
-return (-1);
-}
-write(file, text_content, bytes);
-return (1);
+	int i = 0, file;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+
+	while (text_content[i] != '\0')
+	{
+		i++;
+	}
+
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (file == -1)
+		return (-1);
+
+	write(file, text_content, i);
+
+	return (1);
 }
