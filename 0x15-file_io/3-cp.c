3d3a0e97af8da23fa79d85e9d7428184b8523c28
Moamen-Youssef <113758180+Moamen-Youssef@users.noreply.github.com>
2023-10-03 21:56:51 +0300

    i\o

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,27 +2,27 @@
 #include <stdlib.h>
 /**
  * copy_to_file - check the code
- * @filename1: .....>
- * @filename2: .....>
+ * @file_from: .....>
+ * @file_to: .....>
  * Return: Always 0.
  */
-int copy_to_file(const char *filename1, const char *filename2)
+int copy_to_file(const char *file_from, const char *file_to)
 {
 int file1, file2;
 ssize_t file1_data;
 char buffer[1024];
-file1 = open(filename1, O_RDONLY);
+file1 = open(file_from, O_RDONLY);
 if (file1 == -1)
 {
-fprintf(stderr, "Can't read from file %s\n", filename1);
+fprintf(stderr, "Can't read from file %s\n", file_from);
 exit(98);
 }
-file2 = open(filename2, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+file2 = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 while ((file1_data = read(file1,buffer, sizeof(buffer))) > 0)
 {
 if (write(file2, buffer, file1_data) != file1_data)
 {
-fprintf(stderr, "Can't write to file %s\n", filename2);
+fprintf(stderr, "Can't write to file %s\n", file_to);
 exit(99);
 }
 }
@@ -40,7 +40,7 @@ int main(int ac, char **av)
 int res;
 if (ac != 3)
 {
-fprintf(stderr, "Usage: cp %s %s\n", av[0]);
+fprintf(stderr, "Usage: cp %s %s\n", av[1], av[2]);
 exit(97);
 }
 res = copy_to_file(av[1], av[2]);
