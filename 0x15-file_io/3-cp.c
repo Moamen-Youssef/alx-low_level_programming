f2f70616d4b00235f5edf8cc9d3e09f7361c785e
Moamen-Youssef <113758180+Moamen-Youssef@users.noreply.github.com>
2023-10-03 21:44:10 +0300

    i\o

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * copy_to_file - check the code
+ * @filename1: .....>
+ * @filename2: .....>
+ * Return: Always 0.
+ */
+int copy_to_file(const char *filename1, const char *filename2)
+{
+int file1, file2;
+ssize_t file1_data;
+char buffer[1024];
+file1 = open(filename1, O_RDONLY);
+if (file1 == -1)
+{
+fprintf(stderr, "Can't read from file %s\n", filename1);
+exit(98);
+}
+file2 = open(filename2, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+while (file1_data = read(file1,buffer, sizeof(buffer)) > 0)
+{
+if (write(file2, buffer, file1_data) != file1_data)
+{
+fprintf(stderr, "Can't write to file %s\n", filename2);
+exit(99);
+}
+}
+close(file1);
+if (close(file2) == -1)
+{
+fprintf(stderr, "Can't close fd %s\n", file2);
+exit(100);
+} 
+}
+
+int main(int ac, char **av)
+{
+int res;
+if (ac != 3)
+{
+fprintf(stderr, "Usage: cp %s %s\n", av[1], av[2]);
+exit(97);
+}
+res = copy_to_file(av[1], av[2]);
+return (res);
+}
