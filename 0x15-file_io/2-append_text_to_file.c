0176e8ce85a1b28712bc40d9ed8da40d9b0c9d99
Moamen-Youssef <113758180+Moamen-Youssef@users.noreply.github.com>
2023-10-03 18:21:46 +0300

    i\o

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * append_text_to_file - check the code
+ *@filename: ...<
+ *@text_content: ...<
+ * Return: 0...<
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+int file;
+int bytes = 0;
+if (!filename)
+{
+return (-1);
+}
+while (text_content[bytes] != '\0')
+{
+bytes++;
+}
+file = open(filename, O_WRONLY | O_APPEND, 0644);
+if (!file)
+{
+return (-1);
+}
+if (!text_content)
+{
+close(file);
+return(1);
+}
+write(file, text_content, bytes);
+return (1);
+}
